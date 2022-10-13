406c72b7bc69918ab8872c66a11dd3b46707009e
nsainton <noahsaintonge@gmail.com>
2022-10-13 12:35:20 +0200

    ft_substr

--- Libft/includes/libft.h
+++ Libft/includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: nsainton <nsainton@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 19:32:01 by nsainton          #+#    #+#             */
-/*   Updated: 2022/10/12 17:58:05 by nsainton         ###   ########.fr       */
+/*   Updated: 2022/10/13 12:03:25 by nsainton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,8 @@ char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *s1, const char *s2, size_t n);
 
 char	*ft_strdup(const char *s);
+
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 void	*ft_memset(void *b, int c, size_t len);
 
