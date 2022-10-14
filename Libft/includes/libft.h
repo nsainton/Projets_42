250193256ec24cfaef47b3481724dd5a1c4a821e
nsainton <noahsaintonge@gmail.com>
2022-10-14 16:22:28 +0200

    Added ft_striteri and according test file

--- Libft/includes/libft.h
+++ Libft/includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: nsainton <nsainton@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 19:32:01 by nsainton          #+#    #+#             */
-/*   Updated: 2022/10/14 14:53:14 by nsainton         ###   ########.fr       */
+/*   Updated: 2022/10/14 16:12:30 by nsainton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,4 +69,5 @@ void	*ft_memchr(const void *s, int c, size_t n);
 
 void	*ft_calloc(size_t nmemb, size_t size);
 
+void	ft_striteri(char *s, void (*f) (unsigned int, char *));
 #endif
---
+++ Libft/mains/main_ft_striteri.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_ft_striteri.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsainton <nsainton@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/14 16:14:07 by nsainton          #+#    #+#             */
+/*   Updated: 2022/10/14 16:21:28 by nsainton         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/libft.h"
+#include <stdio.h>
+#include <time.h>
+
+static void	f(unsigned int a, char *s)
+{
+	if (s == NULL)
+		return ;
+	*(s + a) = 100 + (*(s + a) % 20);
+}
+
+static void	ft_test(char *s, void (*f) (unsigned int, char *))
+{
+	clock_t	start;
+	double	cpu_time;
+
+	printf("This is the original string : %s\n", s);
+	start = clock();
+	ft_striteri(s, f);
+	cpu_time = (double)(clock() - start) / CLOCKS_PER_SEC;
+	printf("This is the modified string : %s\n", s);
+	printf("ft_striteri modified the string in %f seconds\n\n\n", cpu_time);
+}
+
+int	main(void)
+{
+	char	c[30] = "Bonjour";
+
+	ft_test(c, f);
+	return (0);
+}
