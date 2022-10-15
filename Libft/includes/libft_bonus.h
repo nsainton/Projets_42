8fb4f3e6d69d23fa9c9a4f7d61b8e691ba7896eb
nsainton <noahsaintonge@gmail.com>
2022-10-15 20:43:11 +0200

    ft_lstmap

--- Libft/includes/libft_bonus.h
+++ Libft/includes/libft_bonus.h
@@ -6,7 +6,7 @@
 /*   By: nsainton <nsainton@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 15:15:47 by nsainton          #+#    #+#             */
-/*   Updated: 2022/10/15 20:32:17 by nsainton         ###   ########.fr       */
+/*   Updated: 2022/10/15 20:42:26 by nsainton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,4 +29,7 @@ void	ft_lstiter(t_list *lst, void (*f) (void *));
 t_list	*ft_lstnew(void *content);
 
 t_list	*ft_lstlast(t_list *lst);
+
+t_list	*ft_lstmap(t_list *lst, void *(*f) (void *), void (*del) (void *));
+
 #endif
