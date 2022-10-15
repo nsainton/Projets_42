14d3c9bf8847c05250a90d4598a8be962083d299
nsainton <noahsaintonge@gmail.com>
2022-10-15 20:27:31 +0200

    ft_lstclear

--- Libft/bonus/sources/ft_lstclear.c
+++ Libft/bonus/sources/ft_lstclear.c
@@ -1,30 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft_bonus.h                                      :+:      :+:    :+:   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nsainton <nsainton@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/15 15:15:47 by nsainton          #+#    #+#             */
-/*   Updated: 2022/10/15 20:26:18 by nsainton         ###   ########.fr       */
+/*   Created: 2022/10/15 20:18:15 by nsainton          #+#    #+#             */
+/*   Updated: 2022/10/15 20:25:34 by nsainton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_BONUS_H
-# define LIBFT_BONUS_H
-# include "libft.h"
+#include "../../includes/libft_bonus.h"
 
-int		ft_lstsize(t_list *lst);
+void	ft_lstclear(t_list **lst, void (*del) (void *))
+{
+	t_list	*p;
+	t_list	*q;
 
-void	ft_lstadd_front(t_list **lst, t_list *new);
-
-void	ft_lstadd_back(t_list **lst, t_list *new);
-
-void	ft_lstdelone(t_list *lst, void (*del) (void *));
-
-void	ft_lstclear(t_list *lst, void (*del) (void *));
-
-t_list	*ft_lstnew(void *content);
-
-t_list	*ft_lstlast(t_list *lst);
-#endif
+	if (lst == NULL)
+		return ;
+	p = *lst;
+	while (p->next != NULL)
+	{
+		q = p->next;
+		del(p->content);
+		free(p);
+		p = q;
+	}
+	free(p);
+	*lst = NULL;
+}
