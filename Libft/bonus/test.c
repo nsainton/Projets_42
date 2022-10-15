acc7911ed6703e1d1dfb8c75ab77aec2e9a19a9f
nsainton <noahsaintonge@gmail.com>
2022-10-15 22:13:17 +0200

    Working on test file for linked lists functions

--- Libft/bonus/test.c
+++ Libft/bonus/test.c
@@ -6,7 +6,7 @@
 /*   By: nsainton <nsainton@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/08 10:36:43 by nsainton          #+#    #+#             */
-/*   Updated: 2022/10/15 21:39:22 by nsainton         ###   ########.fr       */
+/*   Updated: 2022/10/15 22:08:23 by nsainton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,10 @@ static void	delete(void *s)
 	p = (char *)s;
 	*p = 0;
 }
-/*
+
+static void del(void *s)
+{
+	
 static void	print(void *s)
 {
 	char	*p;
@@ -30,19 +33,36 @@ static void	print(void *s)
 		return ;
 	p = (char *)s;
 	ft_putendl_fd(p, 1);
-	ft_putendl_fd("Bonjour", 1);
 }
-*/
+
 int	main(int ac, char **av)
 {
 	t_list	*lst;
 	t_list	*newelem;
+	char	*ns;
 	int		i;
 
 	i = 1;
+	ns = ft_strdup(*av);
+	if (ns == NULL)
+		return (1);
 	lst = ft_lstnew((void *)*av);
 	if (lst == NULL)
 		return (1);
+	while (i < ac)
+	{
+		ns = ft_strdup(*(av + i));
+		newelem = ft_lstnew((void *)ns);
+		if (newelem == NULL)
+		{
+			ft_lstclear(&lst, delete);
+			return (1);
+		}
+		//ft_putendl_fd((char *)newelem->content, 1);
+		ft_lstadd_front(&lst, newelem);
+		i ++;
+	}
+	i = 1;
 	while (i < ac)
 	{
 		newelem = ft_lstnew((void *)*(av + i));
@@ -51,11 +71,12 @@ int	main(int ac, char **av)
 			ft_lstclear(&lst, delete);
 			return (1);
 		}
-		ft_putendl_fd((char *)newelem->content, 1);
 		ft_lstadd_back(&lst, newelem);
 		i ++;
 	}
-	//ft_lstiter(lst, print);
+	ft_lstiter(lst, print);
 	ft_lstclear(&lst, delete);
+	if (lst == NULL)
+		ft_putendl_fd("Completed", 1);
 	return (0);
 }
