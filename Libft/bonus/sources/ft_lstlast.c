e2026051d0cc6dfaed4f717eb71e04f3f6c3ca00
nsainton <noahsaintonge@gmail.com>
2022-10-15 20:02:42 +0200

    added ft_lstlast.c

--- Libft/bonus/sources/ft_lstlast.c
+++ Libft/bonus/sources/ft_lstlast.c
@@ -1,24 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft_bonus.h                                      :+:      :+:    :+:   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nsainton <nsainton@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/15 15:15:47 by nsainton          #+#    #+#             */
-/*   Updated: 2022/10/15 19:51:50 by nsainton         ###   ########.fr       */
+/*   Created: 2022/10/15 19:49:20 by nsainton          #+#    #+#             */
+/*   Updated: 2022/10/15 19:51:15 by nsainton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_BONUS_H
-# define LIBFT_BONUS_H
-# include "libft.h"
+#include "../../includes/libft_bonus.h"
 
-int		ft_lstsize(t_list *lst);
-
-t_list	*ft_lstnew(void *content);
-
-t_list	*ft_lstadd_front(t_list **lst, t_list *new);
-
-t_list	*ft_lstlast(t_list *lst);
-#endif
+t_list	*ft_lstlast(t_list *lst)
+{
+	if (lst == NULL)
+		return (NULL);
+	while (lst->next != NULL)
+		lst = lst->next;
+	return (lst);
+}
