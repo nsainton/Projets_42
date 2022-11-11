dbeb3ba416375aca4d37a8b010584c62cb9de595
poulem <noah.saintonge@dauphine.eu>
2022-11-11 16:42:07 +0100

    Test file to understand printf

---
+++ Printf/test.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsainton <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/10 19:35:10 by nsainton          #+#    #+#             */
+/*   Updated: 2022/11/10 20:04:47 by nsainton         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+int	main(void)
+{
+	int	printed;
+
+	printed = printf("%.1e\n", 12.34);
+	printf("Number of char printed with %%e: %d\n", printed - 1);
+	printed = printf("%.12f\n", 12.34);
+	printf("Number of char printed with %%f: %d\n", printed - 1);
+	printed = printf("%.10g\n", 0.0000123);
+	printf("Number of char printed with %%g : %d\n", printed - 1);
+	return (0);
+}
