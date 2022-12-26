18421efaa0ea7fe9088446e8a9ebed22882a1ae0
nsainton <nsainton@student.42.fr>
2022-12-27 00:23:28 +0100

    Moved all memfuncs to memfuncs.c and removed useless checks for null Same for ft_strlen.c

--- Libft/sources/ft_strlen.c
+++ Libft/sources/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: nsainton <nsainton@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/08 11:17:59 by nsainton          #+#    #+#             */
-/*   Updated: 2022/10/08 15:49:33 by nsainton         ###   ########.fr       */
+/*   Updated: 2022/12/27 00:22:01 by nsainton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ size_t	ft_strlen(const char *s)
 {
 	const char	*ptr;
 
-	if (s == NULL)
-		return (0);
 	ptr = s;
 	while (*ptr)
 		ptr ++;
