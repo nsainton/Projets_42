d10a104ee06e2a911ea1a8523a8f845c19068d85
nsainton <nsainton@student.42.fr>
2022-12-27 03:40:11 +0100

    Removed the echoing of the first argument

--- Scripts/modif.sh
+++ Scripts/modif.sh
@@ -8,7 +8,6 @@ then
 	echo Must provide one argument for the script to work properly
 	exit 3
 fi
-echo $1
 make -C $Libpath
 gcc -Wall -Wextra -Werror $Libpath/libft.a modif.c -o modif
 for i in $path/*
