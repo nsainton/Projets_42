342c03e8b49201420b2af7547e9e3a91bf461edb
nsainton <nsainton@student.42.fr>
2022-12-28 18:05:25 +0100

    Added if condition to tmpget and added option to generate executable file that has the name of the test file

--- Tests/libft/tmpget.sh
+++ Tests/libft/tmpget.sh
@@ -1,1 +1,9 @@
-../../Scripts/getlib.sh ../../Libft libft.a
+#Use the script like : ./Script test_file.c
+o_file="$(basename $1 .c)"
+if [ "$1" == "" ]
+then
+	echo Script is meant to be used by running ./tmpget.sh test_file.c
+	exit 2
+fi
+../../Scripts/getlib.sh ../../Libft libft.a && gcc -Wall -Wextra\
+ -Werror -g3 $1 libft.a -o $o_file
