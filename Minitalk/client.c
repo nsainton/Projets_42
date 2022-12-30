773ab6cf4ca15aa200bc2caad46d364228e6a739
nsainton <nsainton@student.42.fr>
2022-12-30 21:19:04 +0100

    Added protection for server_pid before kill in client.c

--- Minitalk/client.c
+++ Minitalk/client.c
@@ -6,7 +6,7 @@
 /*   By: nsainton <nsainton@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 05:24:05 by nsainton          #+#    #+#             */
-/*   Updated: 2022/12/30 09:37:32 by nsainton         ###   ########.fr       */
+/*   Updated: 2022/12/30 21:18:52 by nsainton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,14 @@ int	main(int ac, char **av)
 		error_handling("Incorrect number of arguments supplied");
 	if (tstr_initstr(&g_message, *(av + 2), 0))
 		error_handling("Allocation Error, what did you do to get that?");
+	tstr_print(&g_message);
 	server_pid = ft_atoi(*(av + 1));
 	action.sa_sigaction = handle_sigusr2;
 	action.sa_flags = SA_SIGINFO;
 	sigemptyset(&action.sa_mask);
 	sigaddset(&action.sa_mask, SIGUSR1);
 	sigaction(SIGUSR2, &action, NULL);
-	if (kill(server_pid, SIGUSR1) == -1)
+	if (server_pid < 1 || kill(server_pid, SIGUSR1) == -1)
 		error_handling("Why do I talk if no one listens?");
 	while (1);
 	return (0);
