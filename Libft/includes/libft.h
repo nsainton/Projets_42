d568fd6f5f8f32fc2a86122c477a176343899590
nsainton <nsainton@student.42.fr>
2022-12-30 21:46:20 +0100

    Moved ansicolorcodes.h to libft and modified libft header accordingly

--- Libft/includes/libft.h
+++ Libft/includes/libft.h
@@ -6,11 +6,12 @@
 /*   By: nsainton <nsainton@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 19:32:01 by nsainton          #+#    #+#             */
-/*   Updated: 2022/12/26 20:33:12 by nsainton         ###   ########.fr       */
+/*   Updated: 2022/12/30 21:45:20 by nsainton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 # include "functions.h"
+# include "ansicolorcodes.h"
 #endif
